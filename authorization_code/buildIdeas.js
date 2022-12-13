3a0d192fc0db2457879f3dce37ec57a21267e72e
Jacob <jacob@jacobs-mbp.lan>
2022-12-13 14:38:43 -0500

    First commit for new auth repository

---
+++ authorization_code/buildIdeas.js
@@ -0,0 +1,8 @@
+/*
+The idea-
+     - How well do you know your music taste?
+     - Ask questions based on the play count of their music (scope: user-top-read)
+     - What's your favorite artist? (scope: user-top-read)
+     - could ask them to pick which of x artists is their favorite and see if they're right
+
+*/
